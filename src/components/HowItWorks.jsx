0fffa8dc6d3fbf2b1484de16259f9febf29170dd
Salvador Poeta <dsmlhidroponia@gmail.com>
2024-11-08 22:35:57 -0600

    Animations to animations

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { chipImg, frameImg, frameVideo } from '../utils'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import { animateWithGsap } from '../utils/Animations'
+import { animateWithGsap } from '../utils/animations'
 
 const HowItWorks = () => {
   const videoRef = useRef()
