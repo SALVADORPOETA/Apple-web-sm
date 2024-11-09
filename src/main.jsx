6d7cb11a947cd0058fc6cafa5c1c9318d3c1b24a
Salvador Poeta <dsmlhidroponia@gmail.com>
2024-11-08 22:27:07 -0600

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://62db22697b970aacd374174fdb52021c@o4507876011868160.ingest.us.sentry.io/4508261006049280',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
